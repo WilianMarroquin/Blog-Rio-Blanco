eab67bda7d819d66768120f62b9737cd75c78d66
Wilian Marroquin <githubwiliangit@gmial.com>
2023-06-24 13:37:53 -0600

    Adaptable movil

---
+++ codigo.js
@@ -0,0 +1,9 @@
+
+
+function aparecer_menu_movil() {
+    var menu_movil = document.getElementById('menu_movil');
+    menu_movil.style.left = "13px";
+}
+function cerrar_menu_movil() {
+    menu_movil.style.left = "-1200px";
+}
